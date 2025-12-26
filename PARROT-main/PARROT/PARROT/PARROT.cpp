f565def06cb46118b40325a22dcb5259fd1d3bf6
dfghaoi <85017723+dfghaoi@users.noreply.github.com>
2025-12-26 23:59:47 +0100

    Add files via upload

---
+++ PARROT-main/PARROT/PARROT/PARROT.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    char text[100];
+	cin.getline(text, 100);
+    cout << text;
+}
